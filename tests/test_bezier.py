7552734493ee64fc7caf751a1e0695aa64160624
MatthewJohnMorris <matt_morris_zz@yahoo.co.uk>
2021-02-14 19:20:03 +0000

    bezier tests

--- tests/test_bezier.py
+++ tests/test_bezier.py
@@ -2,9 +2,44 @@ import pytest
 
 from pyplot.bezier import *
 
-def test_split_at_t():
+def test_split_at_t_0():
 
-    bez = ((0,0), (0,0), (1,1), (1,1))
+    bez = ((0,0), (0,0), (2,2), (2,2))
     
     bez0 = bezier_split_at_t(bez, 0)
-    print(bez0)
+    assert(bez0[0][0] == (0,0))
+    assert(bez0[0][1] == (0,0))
+    assert(bez0[0][2] == (0,0))
+    assert(bez0[0][3] == (0,0))
+    assert(bez0[1][0] == (0,0))
+    assert(bez0[1][1] == (0,0))
+    assert(bez0[1][2] == (2,2))
+    assert(bez0[1][3] == (2,2))
+    
+def test_split_at_t_1():
+
+    bez = ((0,0), (0,0), (2,2), (2,2))
+    
+    bez1 = bezier_split_at_t(bez, 1)
+    assert(bez1[0][0] == (0,0))
+    assert(bez1[0][1] == (0,0))
+    assert(bez1[0][2] == (2,2))
+    assert(bez1[0][3] == (2,2))
+    assert(bez1[1][0] == (2,2))
+    assert(bez1[1][1] == (2,2))
+    assert(bez1[1][2] == (2,2))
+    assert(bez1[1][3] == (2,2))
+
+def test_split_at_t_mid():
+
+    bez = ((0,0), (0,0), (2,2), (2,2))
+    
+    bez_mid = bezier_split_at_t(bez, 0.5)
+    assert(bez_mid[0][0] == (0,0))
+    assert(bez_mid[0][1] == (0,0))
+    assert(bez_mid[0][2] == (0.5,0.5))
+    assert(bez_mid[0][3] == (1,1))
+    assert(bez_mid[1][0] == (1,1))
+    assert(bez_mid[1][1] == (1.5,1.5))
+    assert(bez_mid[1][2] == (2,2))
+    assert(bez_mid[1][3] == (2,2))
