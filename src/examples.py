0fe95c595623ea9cb3807dfd084aec50957d06e6
MatthewJohnMorris <matt_morris_zz@yahoo.co.uk>
2021-02-20 01:32:56 +0000

    Update examples.py

--- src/examples.py
+++ src/examples.py
@@ -651,9 +651,9 @@ def draw_unknown_pleasures_clip(drawing):
             
     #base_count = int(d.default_circle_path_count(10) / 30)
     #shapes = [d.make_circle((100, 100), 50, 5 * base_count), d.make_circle((96, 104), 40, 4 * base_count), d.make_circle((80, 110), 20, 2 * base_count)]
-    shapes = [d.make_circle((100, 100), 50), d.make_circle((96, 104), 40), d.make_circle((80, 110), 20)]
+    shapes = [d.make_circle((100, 100), r) for r in range(10, 50, 2)]
     sf = ShapeFiller(shapes)
-    clipped_polylines = sf.clip(polylines, any=True)
+    clipped_polylines = sf.clip(polylines, any=False)
          
     for polyline in clipped_polylines:
         drawing.add_polyline(polyline)
