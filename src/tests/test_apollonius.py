f5d35459674c35a52379491320e733e71a68925d
MatthewJohnMorris <matt_morris_zz@yahoo.co.uk>
2021-04-14 00:19:24 +0100

    add test cases and basic solver

--- src/tests/test_apollonius.py
+++ src/tests/test_apollonius.py
@@ -1,7 +1,20 @@
-import apollonius
+import pytest
 
+from apollonius import *
 
 def test_simple():
 
-    apollonius.apollonius_calc()
+    c1, c2, c3 = Circle(0, 0, 1), Circle(4, 0, 1), Circle(2, 4, 2)
+    
+    cA = apollonius_solve(c1, c2, c3, 1, 1, 1)
+    
+    assert cA.x == 2.0
+    assert cA.y == 2.1
+    assert cA.r == 3.9
+    
+    cB = apollonius_solve(c1, c2, c3, -1, -1, -1)
+    
+    assert cB.x == pytest.approx(2.0)
+    assert cB.y == pytest.approx(0.8333333333)
+    assert cB.r == pytest.approx(1.1666666667)
 
